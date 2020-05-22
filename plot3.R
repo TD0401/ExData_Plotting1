2eba007d8a19dfdb042b50f642e8c73c9448e17e
Trina Dey <trina.dumca@gmail.com>
2020-05-23 01:06:06 +0530

    code is same, only restrucutred by introducing loadData.R and removing duplicate code from each file

--- plot3.R
+++ plot3.R
@@ -1,36 +1,13 @@
 plotMultiLineGraph <- function(){
-    #creating data directory if not present already 
-    if(!file.exists("data")){
-        dir.create("data")
-    }
-    
-    #downloading zip file if not already present
-    if(!file.exists("data/PlotData.zip")){
-        fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-        download.file(fileurl ,"data/PlotData.zip", method = "curl")
-    }
-    
-    #unzipping if not present already
-    if(!file.exists("data/PlotData/household_power_consumption.txt")){
-        unzip("data/PlotData.zip",exdir ="data/PlotData")
-    }
-    
-    #reading only rows that of 2007-02-01 and 2007-02-02 as per assignment pointers. However the data file has
-    # date mentioned in dd/mm/yyyy format so given accordingly.
-    library(sqldf)
-    powerData <- read.csv.sql("data/PlotData/household_power_consumption.txt" , sep=";" ,
-                              sql = "Select * from file where Date = '1/2/2007' OR Date = '2/2/2007' ")
-    
-    
+    source("loadData.R")
+    loadData()
     #Setting Graphic Device as png file with mentioned height and width
     png(file="plot3.png" , width = 480 , height = 480)
     
     #plotting the line graph for multiple attribute with the desired attributes. first set the plot then add lines
     datetime_vect <- strptime( paste(powerData$Date , powerData$Time) , "%d/%m/%Y %H:%M:%S")
     plot(x= datetime_vect,  y = powerData$Sub_metering_1,
-         type="n", xlab="", ylab = "Energy sub metering" )
-    
-    lines(datetime_vect, powerData$Sub_metering_1,type="l")
+         type="l", xlab="", ylab = "Energy sub metering" )
     lines(datetime_vect, powerData$Sub_metering_2,type="l",col="red")
     lines(datetime_vect, powerData$Sub_metering_3,type="l",col="blue")
     
